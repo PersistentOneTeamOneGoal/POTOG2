852690fcf6ce3c123e6beb55f9ae82d99b728f27
ryanajacobpitogo <ryanjacob.pitogo.20@usjr.edu.ph>
2022-12-11 11:41:37 +0800

    cart

--- banana-peel-shopping/src/pages/cart/Cart.js
+++ banana-peel-shopping/src/pages/cart/Cart.js
@@ -1,33 +1,143 @@
-import React, {  } from 'react';
-import logo from '../../assets/bananapeel-loaders.png';
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import "./Cart.css";
+import logo from "../../assets/logo-bp.png";
+import { BiHomeCircle } from "react-icons/bi"
 
-const Cart = ({cartItems, setCartItems}) => {
+const Cart = ({ cartItems, setCartItems, checkedItems, setCheckedItems, itemTotal, setItemTotal }) => {
+    const itemTot = checkedItems.reduce((total, obj) => (obj.price * obj.qty) + total,0);
+    setItemTotal(itemTot);
+    
     return (
-        <div>
-            {cartItems.length ? 
-                cartItems.map((items) => {
-                    const {id, title, status, type, price, qty, img, desc} = items;
-                    return(
-                        <div>
-                            <button>+</button>
-                            <div>
-                                <img src={img} alt={title} className="item-img" />
-                            </div>
-                            <div>
-                            <div className="item-info">
-                                <header>
-                                    <h4>{title}</h4>
-                                    <h4 className="price">${price}</h4>
-                                </header>
-                            </div>
-                            </div>
-                        </div>
-                    );}
-                ) : <div> empty </div> }
+    <div>
+      <section className="nav-bar sticky">
+        <div className="nav-container">
+          <img className="logo" src={logo} alt="logo"></img>
+          <Link to='/'>
+            <button className="cart cart-btn" data-testid="cart-btn">
+              <BiHomeCircle />
+            </button>
+          </Link>
         </div>
-    );
-}
+      </section>
+      {cartItems.length ? (
+        cartItems.map((item, index) => {
+          return (
+            <CartItem
+              item={item}
+              index={index}
+              cartItems={cartItems}
+              checkedItems={checkedItems}
+              setCartItems={setCartItems}
+              setCheckedItems={setCheckedItems}
+            />
+          );
+        })
+      ) : (
+        <div> Cart is empty. </div>
+      )}
+      {cartItems.length ?
+        <div className="payables-container">
+            <div className="total">
+                Item Total: ${Number(itemTotal).toFixed(2)}
+            </div>
+            {!itemTotal ? 
+            <p className="checkout-text">
+                Checkout
+            </p>
+            :
+            <Link className="checkout-btn" to='/Checkout'>
+                Checkout
+            </Link>}
+        </div>
+        :
+        <></>
+      }
+    </div>
+  );
+};
 
-const CartItem = ({})
+const CartItem = ({ item, index, cartItems, checkedItems, setCartItems, setCheckedItems }) => {
+  const { id, title, checked, status, type, price, qty, img, desc } = item;
+  const [currImg, setCurrImg] = useState(0);
+  const [currIndex, setCurrIndex] = useState(0);
 
-export default Cart;   
+
+  const addCheckedItem = () => {
+    setCurrIndex(index);
+    checkedItems.map((cItem) => {
+      return cItem.id === item.id ? (cartItems.checked = true) : cartItems.checked;
+    }, []);
+    
+    item.qty !== 0 && !cartItems.checked ?
+        setCheckedItems([...checkedItems, item])
+    : setCheckedItems([...checkedItems]);
+
+    if(cartItems.checked){
+        console.log(currIndex);
+        const filterDupe = checkedItems.filter((cItem)=> cItem!==item);
+        const sortItems = filterDupe.sort((item1, item2)=>{return item1.id - item2.id});
+        setCheckedItems(sortItems);
+        cartItems.checked = false;
+    }
+    else{
+    }
+  };
+
+  const removeCartItem = () => {
+    setCurrIndex(index);
+    cartItems[index].qty = 0;
+    cartItems[index].status = false; 
+    
+    const filterCart = cartItems.filter((cItem) => cItem !== item);
+    const filterChecked = checkedItems.filter((cItem) => cItem!==item);
+
+    setCartItems(filterCart);
+    setCheckedItems(filterChecked);
+  }
+  useEffect(() => console.log("The cart after update", cartItems), [cartItems]);
+  useEffect(() => console.log("The checked after update", checkedItems), [checkedItems]);
+
+  return (
+    <div>
+      <button onClick={addCheckedItem}>+</button>
+      <div className="item-img-container">
+        <img
+          src={item.img[currImg]}
+          alt={item.title}
+          className="item-img slipper-img"
+        />
+        {item.img.map((flopImg, index) => {
+          return (
+            <img
+              src={item.img[index]}
+              alt={item.title}
+              onClick={() => {
+                setCurrImg(index);
+              }}
+              className="item-img slipper-img"
+            />
+          );
+        })}
+      </div>
+      <div>
+        <div className="item-info">
+          <header>
+            <h4>{title}</h4>
+            <h4 className="price">Unit Price: ${price}</h4>
+          </header>
+          <p className="item-text desc">{item.desc}</p>
+          <div>
+            <h4>Current item: {item.qty}</h4>
+            <h4 className="price">
+              Total: ${Number((item.price * item.qty).toFixed(2))}
+            </h4>
+          </div>
+        </div>
+      </div>
+      <button onClick={removeCartItem}>-</button>
+    </div>
+  );
+};
+
+export default Cart;
